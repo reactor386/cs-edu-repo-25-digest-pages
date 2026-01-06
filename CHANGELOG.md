385eed198eaa05f4a479153ab573dc3ba77342ed
reactor386 <reactor386@gmail.com>
2026-01-06 17:17:45 +0300

    Создание mvc проекта
     - установлено средство .Net dotnet-ef
     - добавлен пакет EFCore для SQLite
     - добавлен ASP.NET Core MVC проект .Net 10.0
     - добавлен файл CHANGELOG.md
     - обновлен файл README.md

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# cs-edu-repo-25-digest-pages. Changelog
+
+## [v1.0.100] - 2026.01.04
+
+Репозиторий с домашним заданием
+
+ - создание репозитория
+ - обновлен файл README.md
+ - добавлен файл CHANGELOG.md
+ - добавлен ASP.NET Core MVC проект .Net 10.0
+ - добавлен пакет EFCore для SQLite
+ - установлено средство .Net dotnet-ef
---
+++ DigestPages/DigestPages/Program.cs
@@ -0,0 +1,56 @@
+//-
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using AutoMapper;
+
+
+namespace DigestPages;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+        }
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapStaticAssets();
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}")
+            .WithStaticAssets();
+
+        app.Run();
+    }
+}
